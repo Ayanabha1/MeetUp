84f56a00cc0824a163b3c414af3721c24a0604b0
Ayanabha Misra <misrarimbo@gmail.com>
2023-07-16 19:54:49 +0530

    updates before implementing the videochat functionality

--- src/components/MeetFeeds.jsx
+++ src/components/MeetFeeds.jsx
@@ -1,20 +1,35 @@
-import React from "react";
-
-const Video = () => {
-  return (
-    <div className="min-w-[150px] max-w-[470px] sm:w-[48%] bg-white"></div>
-  );
-};
+import React, { useEffect, useRef, useState } from "react";
+import { useDataLayerValue } from "../Datalayer/DataLayer";
 
 const MeetFeeds = () => {
+  const { showError } = useDataLayerValue();
+  const videoRef = useRef(null);
+  const getLocalStream = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    } catch (err) {
+      showError(err);
+    }
+  };
+
+  useEffect(() => {
+    // getLocalStream();
+  }, []);
+
   return (
     <div className="flex justify-center flex-wrap gap-[30px] h-[95%] w-[100%] overflow-hidden mr-[10px]">
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+      {/* <video
+        className={`min-w-[150px] max-w-[470px] sm:w-[48%] object-contain h-[${height}]`}
+        ref={videoRef}
+        autoPlay
+        playsInline
+      /> */}
     </div>
   );
 };
